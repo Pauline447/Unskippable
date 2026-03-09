40008014f2ac22731678689914df1101863b0039
Pauline447 <paulinelechler@gmail.com>
2026-03-09 21:02:24 +0100

    Reviews Appear one after the other

---
+++ Unskippable Project/Assets/Scripts/Ads/AdBehaviour_Screen01.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdBehaviour_Screen01 : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> m_reviewPrefabs;
+    [SerializeField] private List<Transform> m_reviewPos;
+    [SerializeField] private Transform m_parentReview;
+
+    private Coroutine m_reviewsAppearingRoutine;
+    private void Start()
+    {
+        m_reviewsAppearingRoutine = StartCoroutine(ReviewsAppearingRoutine());
+    }
+
+    public void MakeReviewAppear(int reviewInt)
+    {
+        GameObject review = Instantiate(m_reviewPrefabs[reviewInt], m_parentReview);
+        review.transform.position = m_reviewPos[reviewInt].position;
+    }
+
+    private IEnumerator ReviewsAppearingRoutine()
+    {
+        yield return new WaitForSeconds(5f);
+        MakeReviewAppear(0);
+        yield return new WaitForSeconds(5f);
+        MakeReviewAppear(1);
+        yield return new WaitForSeconds(10f);
+        MakeReviewAppear(2);
+    }
+
+}
