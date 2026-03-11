ece5ada21593a778434bdffaedd08a1def8d4641
Pauline447 <paulinelechler@gmail.com>
2026-03-11 19:21:22 +0100

    Loading Bar, some more visuals

--- Unskippable Project/Assets/Scripts/UI/UI_TimerText.cs
+++ Unskippable Project/Assets/Scripts/UI/UI_TimerText.cs
@@ -2,14 +2,18 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class UI_TimerText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI m_countdownText;
     [SerializeField] private float m_timeRemainingValue = 5f;
     [SerializeField] private List<UnityEvent> m_timeEndedEvent;
+
+    [SerializeField] private Slider m_slider;
   
     private float m_timeRemaining;
+    private float m_sliderValue;
     private int m_currentAd = 0;
 
     public int CurrentAd { get => m_currentAd; set => m_currentAd = value; }
@@ -17,6 +21,7 @@ public class UI_TimerText : MonoBehaviour
     private void OnEnable()
     {
         m_timeRemaining = m_timeRemainingValue;
+        m_sliderValue = 0;
     }
 
     void Update()
@@ -24,10 +29,14 @@ public class UI_TimerText : MonoBehaviour
         if (m_timeRemaining > 0)
         {
             m_timeRemaining -= Time.deltaTime;
+            m_sliderValue += Time.deltaTime;
+            m_slider.value = m_sliderValue;
+
             m_countdownText.text = Mathf.Ceil(m_timeRemaining).ToString();
         }
         else
         {
+            m_sliderValue = m_timeRemainingValue;
             m_timeRemaining = 0;
             m_timeEndedEvent[CurrentAd].Invoke();
             CurrentAd++;
