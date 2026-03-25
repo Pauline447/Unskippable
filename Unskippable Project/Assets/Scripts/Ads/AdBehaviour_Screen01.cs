0b99c455d60e4b68f6bc4d8abe460c83a3a23b5a
Pauline447 <paulinelechler@gmail.com>
2026-03-25 11:46:37 +0100

    Inserted New Reviews

--- Unskippable Project/Assets/Scripts/Ads/AdBehaviour_Screen01.cs
+++ Unskippable Project/Assets/Scripts/Ads/AdBehaviour_Screen01.cs
@@ -9,13 +9,13 @@ public class AdBehaviour_Screen01 : AdBehaviour
     protected override IEnumerator AppearingRoutine()
     {
         Debug.Log("Restart Routine");
-        MakeAppear(0);
+        MakeAppear(3);
         yield return new WaitForSeconds(0.5f);
-        MakeAppear(1);
+        MakeAppear(2);
 
         yield return new WaitForSeconds(10f);
 
-        MakeAppear(2);
+        MakeAppear(5);
         yield return new WaitForSeconds(0.5f);
         MakeAppear(3);
 
@@ -25,7 +25,6 @@ public class AdBehaviour_Screen01 : AdBehaviour
         MakeAppear(1);
 
         yield return new WaitForSeconds(8f);
-        MakeAppear(4);
 
         StopCoroutine(m_appearingRoutine);
         m_appearingRoutine = StartCoroutine(AppearingRoutine());
